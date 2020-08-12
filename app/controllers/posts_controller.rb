cfd79d945e59296a8a600eff5c2762311376ce07
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-08-12 11:22:26 +0100

    Add form field that sends to create method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,14 @@
 class PostsController < ApplicationController
 
     def index 
-        
+
+    end
+
+    def new
+
+    end
+
+    def create
+        render plain: params[:post].inspect
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   get 'about' => 'pages#about' 
+
+
+  resource :posts
 end
