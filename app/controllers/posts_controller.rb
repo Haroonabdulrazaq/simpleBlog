26bcf9e680d8afe3d61a10a06858275605e1b355
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-08-12 14:35:58 +0100

    Add new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,11 +4,23 @@ class PostsController < ApplicationController
 
     end
 
+    # def show
+    #   @post = Post.find(params[:id])
+    # end
+
     def new
 
     end
 
     def create
-        render plain: params[:post].inspect
+        render plain: params[:posts].inspect
+        # @post = Post.new(post_params) 
+        
+        # @post.save
+        # redirect_to @post 
     end
+
+    # private def post_params
+    #     params.require(:post).permit(:title,:body)
+    # end
 end
