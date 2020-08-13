3a93819bb26c9bdc6939bf9882d98f2c0d8e915d
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-08-14 00:37:45 +0100

    Getting stuck

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,19 @@ class PostsController < ApplicationController
         
     end
 
+    def show
+        @post = Poster.find(params[title])
+    end
+
     def create
-        @post = Poster.new(params[:post])
+        @post = Poster.new(post_params)
  
         @post.save
-        redirect_to @post
+        redirect_to posts_path(@post)
+    end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :text)
     end
 end
